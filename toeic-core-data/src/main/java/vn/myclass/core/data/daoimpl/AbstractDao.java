9b20fa0cd0d77275fa4ab5eedfcabcc6a5292781
amazingmist <sol4.amazingmist@gmail.com>
2018-09-09 19:01:34 +0700

    Build toeic-core-data layer
    + Add GenericDao.java
    + Add AbstractDao.java
    + Create method findAll in AbstractDao.java

---
+++ toeic-core-data/src/main/java/vn/myclass/core/data/daoimpl/AbstractDao.java
@@ -0,0 +1,53 @@
+package vn.myclass.core.data.daoimpl;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import vn.myclass.core.common.utils.HibernateUtil;
+import vn.myclass.core.data.dao.GenericDao;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AbstractDao<ID extends Serializable, T> implements GenericDao<ID, T> {
+    private Class<T> persistenceClass;
+
+    public AbstractDao() {
+        // generic < x , y > as array
+        // set persistenceClass = T
+        this.persistenceClass = (Class<T>) ((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+    }
+
+    public String getPersistenceClass(){
+        return this.persistenceClass.getSimpleName();
+    }
+
+    protected Session getSession(){
+        return HibernateUtil.getSessionFactory().openSession();
+    }
+    public List<T> findAll() {
+        // TODO: change list = null when declare
+        List<T> list = new ArrayList<T>();
+        Transaction transaction = null;
+
+        try {
+            Session session = this.getSession();
+            transaction = session.beginTransaction();
+
+            StringBuilder hql = new StringBuilder("from ");
+            hql.append(this.getPersistenceClass());
+
+            Query query = session.createQuery(hql.toString());
+            list = query.list();
+            transaction.commit();
+        }catch (HibernateException ex){
+            transaction.rollback();
+            throw ex;
+        }
+
+        return list;
+    }
+}
