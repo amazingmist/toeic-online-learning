45882465c0f47430132502706f4f9ce32d6a1e5f
amazingmist <sol4.amazingmist@gmail.com>
2018-09-09 11:27:57 +0700

    Create entities
    + Create Role.java
    + Create User.java
    + Mapping 2 entities with database
    + Set relation between 2 entities

---
+++ toeic-core-persistence-data/src/main/java/vn/myclass/core/persistence/entity/Role.java
@@ -0,0 +1,36 @@
+package vn.myclass.core.persistence.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id     // mark as primary key
+    @Column(name = "roleid")    // mapping with column in table
+    private short roleId;
+
+    @Column(name = "name")
+    private String name;
+
+    // one role to many user
+    // mappedBy is the same with @JoinColumn in User.java
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    private List<User> userList;
+
+    public short getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(short roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
