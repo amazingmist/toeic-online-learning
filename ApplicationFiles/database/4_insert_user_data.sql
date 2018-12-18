0f2654cf25c2a652af4065216b91abaa4c680cb7
Võ Thành Tài <taivtse@gmail.com>
2018-12-18 22:42:07 +0700

    Add sql file

--- ApplicationFiles/database/4_insert_user_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user VALUES (NULL, "thanhtai", "12345", "Võ Thành Tài", CURRENT_TIMESTAMP, 1);
-INSERT INTO user VALUES (NULL, "baochau", "12345", "Hồ Ngọc Châu", CURRENT_TIMESTAMP, 2);
---
+++ ApplicationFiles/database/5_insert_user_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user VALUES (NULL, "thanhtai", "12345", "Võ Thành Tài", CURRENT_TIMESTAMP, 1);
+INSERT INTO user VALUES (NULL, "baochau", "12345", "Hồ Ngọc Châu", CURRENT_TIMESTAMP, 2);
